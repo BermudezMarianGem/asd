252ab481c16a13e78ab3e098880a5cd33a5fa51c
Marian Gem Bermudez <mariangembermudez1207@gmail.com>
2022-10-17 21:53:11 +0800

    Updated Login and Regsiter page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,14 +3,13 @@ import {useHistory, Link} from 'react-router-dom';
 
 const Login = () =>
 {
-    const [email, setEmail]=useState("");
+    const [username, setUsername]=useState("");
     const [password, setPassword]=useState("");
     const history = useHistory();
 
     async function login()
     {
-        console.warn(email,password)
-        let item=(email,password);
+        let item={username,password};
         let result = await fetch("http://localhost:8000/api/login",{
             method: "POST",
             headers: {
@@ -22,7 +21,16 @@ const Login = () =>
         });
         result = await result.json();
         localStorage.setItem("user-info",JSON.stringify(result))
-        history.push("/add-product")
+        
+        if ("error" in result)
+        {
+            alert("Error Message!")
+        }
+        else
+        {
+            history.push("/add-product")
+        }
+        
     }
     return(
         <div>
@@ -30,8 +38,8 @@ const Login = () =>
             <div className="col-sm-6 offset-sm-3">
                 <h1>Seller Login Page</h1>
                 <p>Login to continue</p>
-                <input type ="text" placeholder="email" onChange={(e) => setEmail(e.target.value)} className="form-control"/><br/>
-                <input type ="password" placeholder="password" onChange={(e) => setPassword(e.target.value)}className="form-control"/><br/>
+                <input type ="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} className="form-control"/><br/>
+                <input type ="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}className="form-control"/><br/>
 
                 <button onClick={login}className="btn btn-primary">Login</button>
                 <p>Don't have account? <Link to ="/register">Register here</Link></p>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,15 +3,18 @@ import {useHistory, Link} from 'react-router-dom';
 
 const Register = () => {
 
-    const [name, setName]=useState("")
-    const [password, setPassword]=useState("")
+    const [firstname, setFirstname]=useState("")
+    const [middlename, setMiddlename]=useState("")
+    const [lastname, setLastname]=useState("")
+    const [username, setUsername]=useState("")
     const [email, setEmail]=useState("")
+    const [password, setPassword]=useState("")
     const history = useHistory();
 
     async function signUp()
     {
         
-        let item={name,password,email}
+        let item={firstname, middlename, lastname, username, email, password}
         console.warn(item)
 
         let result = await fetch("http://localhost:8000/api/register",
@@ -32,10 +35,12 @@ const Register = () => {
         <div className="col-sm-6 offset-sm-3">
            <h1>CREATE ACCOUNT</h1>
            <p>SIgn-up as a seller</p>
-           <input type="text" value={name} onChange={(e)=>setName(e.target.value)} className="form-control" placeholder="name" /><br/>
-           <input type="password" value={password}  onChange={(e)=>setPassword(e.target.value)} className="form-control" placeholder="password" /><br/>
+           <input type="text" value={firstname} onChange={(e)=>setFirstname(e.target.value)} className="form-control" placeholder="firstname" /><br/>
+           <input type="text" value={middlename} onChange={(e)=>setMiddlename(e.target.value)} className="form-control" placeholder="middlename" /><br/>
+           <input type="text" value={lastname} onChange={(e)=>setLastname(e.target.value)} className="form-control" placeholder="lastname" /><br/>
+           <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} className="form-control" placeholder="username" /><br/>
            <input type="email" value={email}  onChange={(e)=>setEmail(e.target.value)} className="form-control" placeholder="email" /><br/>
-
+           <input type="password" value={password}  onChange={(e)=>setPassword(e.target.value)} className="form-control" placeholder="password" /><br/>
            <button onClick = {signUp} className="btn btn-primary">Register</button>
            <p>Already have an account?<Link to ="/login">Login here</Link></p>
         </div>
