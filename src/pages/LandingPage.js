bdb2284e06f28189ae2559a1747a50b087085752
Althea <al.baculi@mseuf.edu.ph>
2022-11-04 21:11:00 +0800

    Updated design of Login,register,landing and selection pages.

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import NavbarLandingPage from './NavbarLandingPage';
 import logo2 from '../pages/images/landinglogo.png';
+import logo from '../pages/images/logo.png';
 
 function LandingPage() 
 {
     return (
         <>
-        <NavbarLandingPage/>
-        <div className = "container">
-            <div className='row'>
-                <div className='col'>
-                    <p style={{fontSize: 90, fontWeight: 'bold', color: "#388E3C" }}>OPEN YOUR<br/>STORE</p>
-                    <p style={{ fontSize: 20, color: '#388E3C', wordSpacing: 7, }}>Manage your shop efficiently on<br/>Agrikonnect with our AgriKonnect <br/>Farmer Center.</p>
-                    <button style={{ padding: 20, backgroundColor: "#388E3C",   }}><Link to={'/selectionpage'}> GET STARTED</Link></button>
+        <header className="header">
+            <div className="wrap">
+            <header className="header-title">
+                <div className='title-logo'>
+                    <h1 className="logo-title">
+                    <img src={logo}  alt="logo" width={100} height={100}></img>
+                    AgriKOnnect
+                    </h1>
                 </div>
-                
-                <div className='col'>
-                    <img src={logo2} alt=""/>
-                </div>
-                
+            </header>
+            </div>
+        </header>
+        <div className="column-left">
+            <div className="content1">
+                <p>OPEN YOUR <br></br> STORE</p>
+            </div>
+            <div className="content">
+            <p> Manage your shop efficiently on <br></br>
+                Agrikonnect with our AgriKonnect<br></br> Farmer Center. </p>
+            </div>
+            <div className="button">
+            <button className="bttn"><Link to={'/selectionpage'}> GET STARTED </Link></button>
             </div>
         </div>
-        </>   
+        <div className="column-right">
+            <div className="icon-ecommerce">
+            <img src={logo2} alt="illustration" width={850} height={850}></img>
+            </div>
+        </div>
+        </> 
     );
 
 }
--- src/pages/SelectionPage.js
+++ src/pages/SelectionPage.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import NavbarLandingPage from './NavbarLandingPage'
+import logo from '../pages/images/logo.png';
+import logo2 from '../pages/images/landinglogo.png';
+
 function SelectionPage() 
 {
     return (
         <>
-        <NavbarLandingPage/>
-        <div>
-            <div className='content'>
-                <div className='contentText'>
-                    <p>What kind of user are you?</p>
+        <header className="header">
+            <div className="wrap">
+            <header className="header-title">
+                <div className='title-logo'>
+                    <h1 className="logo-title">
+                    <img src={logo}  alt="logo" width={100} height={100}></img>
+                    AgriKOnnect
+                    </h1>
                 </div>
-                <Link to={'/login-seller'} className="btn btn-primary btn-sm float-start"> Seller</Link>
-                <Link to={'/login-customer'} className="btn btn-primary btn-sm float-start"> Customer</Link>
+            </header>
+            </div>
+        </header>
+        <div className="column-left">
+            <div className="content2">
+                <p>What kind of user <br></br> are you?</p>
+            </div>
+            <div className="button">
+            <button className="bttn-seller"><Link to={'/login-seller'}> SELLER </Link></button><br></br>
+            <button className="bttn-customer"><Link to={'/login-customer'}> CUSTOMER </Link></button>
+            </div>
+        </div>
+        <div className="column-right">
+            <div className="icon-ecommerce">
+            <img src={logo2} alt="illustration" width={700} height={700}></img>
             </div>
         </div>
         </>
